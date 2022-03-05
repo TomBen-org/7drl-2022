e155151588e150238337153615aa4ed949d25523
Ben Buckton <contact@benbuckton.com>
2022-03-05 14:35:18 +0100

    movement!

--- Assets/MovementRaycaster.cs
+++ Assets/MovementRaycaster.cs
@@ -3,46 +3,89 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MovementRaycaster : MonoBehaviour
-{
-    public enum TurnMode {
-        Start,
-        MoveSelect,
-        ActionSelect,
-        Moving,
-        End
-    }
-
-    public TurnMode currentMode;
+public class MovementRaycaster : MonoBehaviour {
+    private const float _acceptanceClickRange = 0.25f;
+    
     public Camera camera;
+
+    public Vector2 targetPoint = Vector2.negativeInfinity;
     
     private LineRenderer _lineRenderer;
+    private TurnDirector _director;
 
     private void Awake() {
         _lineRenderer = transform.Find("LineRenderer").GetComponent<LineRenderer>();
+        _director = GetComponent<TurnDirector>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void PhaseUpdate()
     {
-        if (currentMode == TurnMode.MoveSelect && Input.GetMouseButton(0)) {
+        if (Input.GetKeyDown(KeyCode.Space) && IsTargetPointValid()) {
+            Debug.Log("Next phase accepted by spacebar");
+            _director.NextPhase();
+            return;
+        }
+        
+        if (Input.GetMouseButton(0)) {
+            var worldMousePos = camera.ScreenToWorldPoint(Input.mousePosition);
+
+            //move on if the player clicks in the same place as an existing, valid location.
+            if (IsTargetPointValid() && Vector2.Distance(targetPoint, worldMousePos) < -_acceptanceClickRange) {
+                Debug.Log("Next phase accepted");
+                _director.NextPhase();
+                return;
+            }
+
             //shoot a ray at the mouse
-            Vector2 targetDirection = camera.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+            Vector2 targetDirection = worldMousePos - transform.position;
 
             int mask = LayerMask.GetMask("MoveTarget", "Enemy");
-            
-            RaycastHit2D hit = Physics2D.Raycast(transform.position, targetDirection,5000f, mask);
+
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, targetDirection, 5000f, mask);
 
             if (hit.collider) {
-                _lineRenderer.SetPositions(new Vector3[]{transform.position,hit.point});
+                _lineRenderer.SetPositions(new Vector3[] {transform.position, hit.point});
                 if (hit.collider.transform.GetComponent<Enemy>() != null) {
                     _lineRenderer.endColor = Color.red;
+                    targetPoint = Vector2.negativeInfinity;
                 }
                 else {
                     _lineRenderer.endColor = Color.green;
+                    targetPoint = hit.point;
                 }
             }
-            
+            else {
+                targetPoint = Vector2.negativeInfinity;
+            }
         }
+
+        if (Input.GetMouseButtonUp(0)) {
+            //set the end turn button visible based on validity.
+            if (IsTargetPointValid()) {
+                Debug.Log("valid position selected");
+            }
+        }
+
+        if (Input.GetMouseButtonUp(1)) {
+            ResetTargetPoint();
+        }
+    
+
+    }
+    
+    public bool IsTargetPointValid() {
+        return targetPoint != Vector2.negativeInfinity;
+    }
+
+    public void ResetTargetPoint() {
+        Debug.Log("reset line");
+        targetPoint = Vector2.negativeInfinity;
+        _lineRenderer.SetPositions(new Vector3[]{transform.position,transform.position});
+    }
+
+    public Vector2 GetNextPosition() {
+        Vector2 nearPoint =  targetPoint - (Vector2) transform.position;
+        nearPoint *= 0.99f;
+        return IsTargetPointValid() ? (nearPoint + (Vector2) transform.position) : transform.position;
     }
 }
---
+++ Assets/TurnDirector.cs
@@ -0,0 +1,82 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnDirector : MonoBehaviour
+{
+    public enum Phase {
+        Start,
+        MoveSelect,
+        ActionSelect,
+        Moving,
+        End
+    }
+    
+    public Phase currentPhase;
+    public int currentTurn;
+
+    private MovementRaycaster _caster;
+
+    private void Awake() {
+        _caster = GetComponent<MovementRaycaster>();
+    }
+
+    private void Update() {
+        switch (currentPhase) {
+            case Phase.Start:
+                Debug.Log("Beginning MoveSelect Phase");
+                NextPhase();
+                break;
+            case Phase.MoveSelect:
+                _caster.PhaseUpdate();
+                break;
+            case Phase.ActionSelect:
+                Debug.Log("Skipping ActionSelect Phase");
+                NextPhase();
+                break;
+            case Phase.Moving:
+                Debug.Log("Moving Player");
+                transform.position = _caster.GetNextPosition();
+                NextPhase();
+                break;
+            case Phase.End:
+                Debug.Log("Starting a new Turn");
+                EndTurn();
+                break;
+            default:
+                break;
+        }
+        
+    }
+
+    public void NextPhase() {
+        switch (currentPhase) {
+            case Phase.Start:
+                currentPhase = Phase.MoveSelect;
+                break;
+            case Phase.MoveSelect:
+                currentPhase = Phase.ActionSelect;
+                break;
+            case Phase.ActionSelect:
+                currentPhase = Phase.Moving;
+                break;
+            case Phase.Moving:
+                transform.position = _caster.GetNextPosition();
+                currentPhase = Phase.End;
+                break;
+            case Phase.End:
+                EndTurn();
+                break;
+            default:
+                break;
+        }
+    }
+    
+    private void EndTurn() {
+        //refresh player abilities
+        _caster.ResetTargetPoint();
+        currentTurn++;
+        currentPhase = Phase.Start;
+    }
+}
